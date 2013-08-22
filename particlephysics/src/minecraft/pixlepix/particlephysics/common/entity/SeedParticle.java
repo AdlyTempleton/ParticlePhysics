378aca27d920700b22022bb20fc2544c6abfe92e
pixlepix <adlytempleton@gmail.com>
2013-08-22 11:50:03 -0400

    Actually added the api :)

---
+++ particlephysics/src/minecraft/pixlepix/particlephysics/common/entity/SeedParticle.java
@@ -0,0 +1,32 @@
+package pixlepix.particlephysics.common.entity;
+
+import net.minecraft.block.Block;
+import net.minecraft.world.World;
+import pixlepix.particlephysics.common.api.BaseParticle;
+import pixlepix.particlephysics.common.helper.ClientProxy;
+import pixlepix.particlephysics.common.render.BlockRenderInfo;
+
+public class SeedParticle extends BaseParticle {
+
+	public SeedParticle(World par1World) {
+		super(par1World);
+	}
+
+	@Override
+	public float getStartingPotential() {
+		return 200;
+	}
+
+	@Override
+	public BlockRenderInfo getRenderIcon() {
+		// TODO Auto-generated method stub
+		return new BlockRenderInfo(ClientProxy.seed);
+	}
+
+	@Override
+	public void onCollideWithParticle(BaseParticle particle) {
+		particle.potential=(float) Math.max(particle.potential*1.1, particle.getStartingPotential()*2);
+		
+	}
+
+}
