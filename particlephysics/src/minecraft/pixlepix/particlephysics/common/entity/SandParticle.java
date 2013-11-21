8be678eed12cf28040085c5126c6a32bc6ed87a5
pixlepix <adlytempleton@gmail.com>
2013-11-20 21:35:16 -0500

    Nerfed sand particles

--- particlephysics/src/minecraft/pixlepix/particlephysics/common/entity/SandParticle.java
+++ particlephysics/src/minecraft/pixlepix/particlephysics/common/entity/SandParticle.java
@@ -16,7 +16,7 @@ public class SandParticle extends BaseParticle {
 		super(par1World);
 		// TODO Auto-generated constructor stub
 	}
-	public ArrayList<CoordTuple> previous=new ArrayList<CoordTuple>();
+	public int genLeft=4;
 	@Override
 	public float getStartingPotential() {
 		// TODO Auto-generated method stub
@@ -37,12 +37,12 @@ public class SandParticle extends BaseParticle {
 	@Override
 	public void onBounceHook(int x,int y, int z){
 		CoordTuple newTuple=new CoordTuple(x,y,z);
-		if(previous.contains(newTuple)){
+		if(genLeft==0){
 			return;
 		}
-		previous.add(newTuple);
+		genLeft--;
 		SandParticle reflectedParticle=new SandParticle(this.worldObj);
-		reflectedParticle.previous=this.previous;
+		reflectedParticle.genLeft=this.genLeft;
 		ForgeDirection dir=this.movementDirection.getOpposite();
 		reflectedParticle.setVelocity(dir.offsetX,dir.offsetY,dir.offsetZ);
 		reflectedParticle.setPosition(this.posX,this.posY,this.posZ);
